2a2396b57ca7a48474e3ae695d7fabf18cb888ad
Javier <65828792+jisantillan@users.noreply.github.com>
2022-07-29 18:22:26 -0300

    Ejercicio 2

---
+++ js-2/index.js
@@ -0,0 +1,10 @@
+const fechaNacimiento = new Date(2022, 0 , 19);
+const libro = {
+    titulo: "blaze",
+    autor: "Stephen King",
+    url: "https://es.wikipedia.org/wiki/Blaze_(novela)",
+    fecha: new Date(2007,5,12)
+}
+
+const arr = ["javier",25,true,fechaNacimiento,libro];
+console.log(arr);
