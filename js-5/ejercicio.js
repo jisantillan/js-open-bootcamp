cb8b7ee8d988651a1b01ce6cde04073bd7a34828
Javier <65828792+jisantillan@users.noreply.github.com>
2022-08-01 14:09:56 -0300

    Ejercicio 5 - Numbers

---
+++ js-5/ejercicio.js
@@ -0,0 +1,6 @@
+let alturaCm = 180;
+let alturaMetro = 1.82;
+let peso = 80.5;
+let alturaRedondArriba = Math.ceil(alturaMetro);
+let pesoRedondAbajo = Math.floor(peso);
+let bool = Number.MAX_VALUE+1 === Number.MAX_VALUE;
