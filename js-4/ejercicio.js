810078c6c1864e6bf9e712d6f539566567c5d4c3
Javier <65828792+jisantillan@users.noreply.github.com>
2022-07-31 21:18:29 -0300

    Ejercicio 4 - Strings

---
+++ js-4/ejercicio.js
@@ -0,0 +1,12 @@
+let nombre = "Javier";
+let apellido = "Santillan";
+let estudiante = nombre.concat(" ",apellido);
+let estudianteMayus = estudiante.toUpperCase();
+let estudianteMinus = estudiante.toLowerCase();
+let cantCaracteresEstudiante = estudiante.length;
+let primerLetraNombre = estudiante.charAt(0);
+let primerLetraApellido = estudiante.charAt(estudiante.length-1);
+let estudianteSinEspacio = estudiante.replace(' ','');
+let nombreEstaEnEstudiante = estudiante.includes(nombre);
+
+
